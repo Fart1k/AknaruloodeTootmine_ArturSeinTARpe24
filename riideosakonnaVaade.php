11732c94e5290064facaf4ebe97ad7669e520e34
ArturSein <seinartur@gmail.com>
2026-01-27 14:46:29 +0200

    Riide osakonna vaade on tehtud, selle osakonna osa märimine valmiks on tehtud

---
+++ riideosakonnaVaade.php
@@ -0,0 +1,51 @@
+<?php
+include "conf.php";
+global $yhendus;
+
+/* Riideosa märkimine valmiks */
+if (isset($_GET['valmis'])) {
+    $id = (int)$_GET['valmis'];
+
+    $paring = $yhendus->prepare(
+        "UPDATE rulood SET riievalmis = 1 WHERE id = ?"
+    );
+    $paring->bind_param("i", $id);
+    $paring->execute();
+}
+?>
+
+<link rel="stylesheet" href="style.css">
+
+<h2>Lõikamata riideosaga tellimused</h2>
+
+<table>
+    <tr>
+        <th>ID</th>
+        <th>Mustri number</th>
+        <th>Riideosa</th>
+        <th>Puuvalmis</th>
+        <th>Pakitud</th>
+        <th>Tegevus</th>
+    </tr>
+
+    <?php
+    /* Tabeli kuvamine */
+    $paring = $yhendus->prepare(
+        "SELECT id, mustrinr, riievalmis, puuvalmis, pakitud FROM rulood WHERE riievalmis IS NULL"
+    );
+
+    $paring->execute();
+    $paring->bind_result($id, $mustrinr, $riievalmis, $puuvalmis, $pakitud);
+
+    while ($paring->fetch()) {
+        echo "<tr>";
+        echo "<td>".htmlspecialchars($id)."</td>";
+        echo "<td>".htmlspecialchars($mustrinr)."</td>";
+        echo "<td>lõikamata</td>";
+        echo "<td>".($puuvalmis ? "valmis" : "tegemisel")."</td>";
+        echo "<td>".($pakitud ? "jah" : "ei")."</td>";
+        echo "<td><a href='?valmis=$id'>Valmis</a></td>";
+        echo "</tr>";
+    }
+    ?>
+</table>
