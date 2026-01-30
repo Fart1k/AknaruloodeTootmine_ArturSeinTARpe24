f2865e1cf982d618bfa1632f8264f5b0131bc655
ArturSein <seinartur@gmail.com>
2026-01-30 09:46:59 +0200

    Navigeerimismenüü ja style uuendamine

--- navigation.php
+++ navigation.php
@@ -1,6 +1,16 @@
-<nav>
-    <a href="tellimuseLisamine.php">Uus tellimus</a>
-    <a href="riideosakonnaVaade.php">Riideosakonna leht</a>
-    <a href="puuOsakonnaVaade.php">Puuosakonna leht</a>
-    <a href="komplekteerijateVaade.php">Komplekteerijate leht</a>
+<nav class="menu">
+    <ul>
+        <li>
+            <a href="tellimuseLisamine.php">Tellimused</a>
+        </li>
+        <li>
+            <a href="riideosakonnaVaade.php">Riideosakonna leht</a>
+        </li>
+        <li>
+            <a href="puuOsakonnaVaade.php">Puuosakonna leht</a>
+        </li>
+        <li>
+            <a href="komplekteerijateVaade.php">Komplekteerijate leht</a>
+        </li>
+    </ul>
 </nav>
