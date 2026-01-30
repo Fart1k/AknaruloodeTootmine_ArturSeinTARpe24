46f271847f3413b6abc96c48fea1eed0028ec8c0
ArturSein <seinartur@gmail.com>
2026-01-30 09:33:33 +0200

    Tellimuse staatuse vaatamine on lisatud

--- tellimuseLisamine.php
+++ tellimuseLisamine.php
@@ -17,10 +17,65 @@ global $yhendus;
 
 <?php
 // tellimuste salvestamine
-    if (isset($_REQUEST['mustrinr'])) {
+    if (isset($_POST['mustrinr'])) {
+        $mustrinr = (int)$_REQUEST['mustrinr'];
+
         $paring = $yhendus->prepare(
             "INSERT INTO rulood (mustrinr) VALUES (?);"
         );
-        $paring->bind_param("i", $_REQUEST['mustrinr']);
+        $paring->bind_param("i", $mustrinr);
         $paring->execute();
+
+        $uuePakkiId = $yhendus->insert_id;
+
+        echo "<p>Tellimus on lisatud</p>";
+        echo "<p><strong>Sinu tellimuse ID: $uuePakkiId</strong></p>";
     }
+?>
+
+<hr>
+
+<h2>Vaata oma paki staatust</h2>
+
+<form action="?" method="post">
+    <label>Sinu pakki ID: </label>
+    <input type="number" name="pakkiId">
+
+    <input type="submit" value="Vaata">
+</form>
+
+<?php
+// tellimuste näitamine
+    if (isset($_POST['pakkiId'])) {
+        $pakkiId = (int)$_POST['pakkiId'];
+
+        $paring = $yhendus->prepare(
+            "SELECT * FROM rulood WHERE id=?;"
+        );
+        $paring->bind_param("i", $pakkiId);
+        $paring->execute();
+
+        $paring->bind_result($id, $mustrinr, $riievalmis, $puuvalmis, $pakitud);
+
+        if ($paring->fetch()) {
+            echo "<table>";
+            echo "<tr>
+                    <th>ID</th>
+                    <th>Muster</th>
+                    <th>Riie</th>
+                    <th>Puu</th>
+                    <th>Pakitud</th>
+                  </tr>";
+            echo "<tr>";
+            echo "<td>$id</td>";
+            echo "<td>$mustrinr</td>";
+            echo "<td>" . ($riievalmis ? "Valmis" : "Tegemisel") . "</td>";
+            echo "<td>" . ($puuvalmis ? "Valmis" : "Tegemisel") . "</td>";
+            echo "<td>" . ($pakitud ? "Jah" : "Ei") . "</td>";
+            echo "</tr>";
+            echo "</table>";
+        } else {
+            echo "<p>Tellimust ei leitud.</p>";
+        }
+    }
+?>
